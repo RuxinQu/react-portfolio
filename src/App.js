ccd387cd4c12192ef955a76d083c434e839b8870
Ruxin Qu <rosheenqu@gmail.com>
2023-01-09 00:46:57 -0800

    add about me section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import { NavbarContainer } from "./container/NavbarContainer";
+import { AboutMe } from "./component/About";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavbarContainer />
-      
+      <AboutMe/>
     </div>
   );
 }
--- src/component/About.js
+++ src/component/About.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
+import Avatar from "avataaars2";
 
-export const AboutMe = ()=>{
-    <Avatar
-        avatarStyle='Circle'
-        topType='LongHairNotTooLong'
-        accessoriesType='Blank'
-        hairColor='Black'
-        facialHairType='Blank'
-        clotheType='BlazerSweater'
-        eyeType='Default'
-        eyebrowType='Default'
-        mouthType='Smile'
-        skinColor='Light'
+export const AboutMe = () => {
+  return (
+    <div id="about-me" className="container-fluid d-flex justify-content-center align-items-center flex-wrap">
+      <Avatar
+        avatarStyle="Circle"
+        topType="LongHairNotTooLong"
+        accessoriesType="Blank"
+        hairColor="Black"
+        facialHairType="Blank"
+        clotheType="BlazerSweater"
+        eyeType="Default"
+        eyebrowType="Default"
+        mouthType="Smile"
+        skinColor="Light"
       />
-}
+      <div className="col-8 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-white py-5">Hi, I'm Ruxin Qu!</h1>
+        <p >
+          I am a Web Developer with a passion to write clean and maintainable
+          code. <br/>I'm currently in Full Stack Coding Bootcamp through University
+          of Washington, <br/>and I have a background M.S degree in computational
+          chemistry.
+        </p>
+      </div>
+    </div>
+  );
+};
