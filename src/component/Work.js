11ae5a4bedf1b12aa77d29b83960505db0723d22
Ruxin Qu <rosheenqu@gmail.com>
2023-01-09 14:32:19 -0800

    style the page to be responsive

--- src/component/Work.js
+++ src/component/Work.js
@@ -9,7 +9,7 @@ export const Work = () => {
         <div className="row d-flex justify-content-center">
           {workData.map((work) => {
             return (
-              <div className="col-12 col-md-3 project m-1 py-2">
+              <div className="col-11 col-md-5 col-lg-3 project m-1 py-2">
                 <h4 className="text-center mb-0 mt-2">{work.title}</h4>
                 <img className="w-100" src={work.img} alt={work.alt} />
                 <div className="d-flex justify-content-around">
