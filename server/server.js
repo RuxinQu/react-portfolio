6c8b3cfcf1cfcd3dcf4108b3545194cbbb20ada3
Ruxin Qu <rosheenqu@gmail.com>
2023-01-25 19:18:48 -0800

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const cors = require("cors");
 const { transporter, router } = require("./api/email");
+
 const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors)
 
-app.use(router)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
@@ -15,6 +27,4 @@ app.listen(port, () => {
       ? console.log(err)
       : console.log(`=== Server is ready to take messages: ${success} ===`);
   });
-
-
 });
