d408e6b0b898045014f146f5e0f1652f9723bf93
Ruxin Qu <rosheenqu@gmail.com>
2023-04-24 13:33:59 -0400

    update project

--- client/src/data.js
+++ client/src/data.js
@@ -1,17 +1,17 @@
 export const workData = [
   {
-    title: "Wei Full",
-    img: "./assets/wei-full.png",
-    alt: "wei-full",
-    page: "https://ruxinqu.github.io/wei-full/",
-    repo: "https://github.com/RuxinQu/wei-full",
-  },
-  {
     title: "Raining Cats and Dogs",
     img: "./assets/raining-cats-and-dogs.png",
     alt: "raining-cats-and-dogs",
     page: "https://raining-dog.herokuapp.com/",
     repo: "https://github.com/RuxinQu/Raining-Cats-and-Dogs",
+  },
+  {
+    title: "Safe Harbor Pet Shelter",
+    img: "./assets/safe-harbor-pet-shelter.png",
+    alt: "safe-harbor-pet-shelter",
+    page: "https://safe-harbor-pet-shelter.herokuapp.com",
+    repo: "https://github.com/RuxinQu/safe-harbor-pet-shelter",
   },
   {
     title: "FitHub",
